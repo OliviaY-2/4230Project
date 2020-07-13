ec5b037da5ab351360d7c51fc51bd440c50861d0
Lucas Way <lucasway98@gmail.com>
2020-07-14 00:23:01 +1000

    started testing YOLO computer vision

---
+++ YOLOv3.m
@@ -0,0 +1,16 @@
+% YOLO v3 Deep Learning
+% Author: Lucas Way z5164204
+% First made (DD/MM/YYY): 13/07/2020
+% Last Edited: 13/07/2020
+% 
+%{
+Taking a .mat file created using ROS_Connect.m, the objects in the image
+will be classified using a trained neural network
+https://au.mathworks.com/help/vision/examples/object-detection-using-yolo-v3-deep-learning.html
+
+Instructions:
+    ---
+
+13/07/2020 created file
+
+%}
