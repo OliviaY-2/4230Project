993c095a29149072841a81c7e6d4cec270446963
Lucas Way <lucasway98@gmail.com>
2020-07-25 15:19:50 +1000

    add comments

--- Convert2PNG.m
+++ Convert2PNG.m
@@ -6,20 +6,22 @@ Taking a .mat file created using ROS_Connect.m, the image will be converted
 to a png and saved in a new file
 
 Reference:
-
+    https://matlab.fandom.com/wiki/FAQ#How_can_I_process_a_sequence_of_files.3F
 Instructions:
-    ---
+    - change the name for 'myFolder' so that it finds the .mat files in the
+    desired folder
+    - change the name for 'PNGFolder' so that it saves the .png in the new
+    folder
 
 Edit History:
-25/07/2020 created file
-
+    25/07/2020 created file
 
 %}
 close all;
 
 %%
 % Specify the folder where the files live.
-myFolder = 'C:\Users\User\Documents\UNSW\MTRN4230\Git Repo\4230Project\RGBD_Data\Red Cube (New)';
+myFolder = 'C:\Users\User\Documents\UNSW\MTRN4230\Git Repo\4230Project\RGBD_Data\Multiple Objects';
 % Check to make sure that folder actually exists.  Warn user if it doesn't.
 if ~isfolder(myFolder)
     errorMessage = sprintf('Error: The following folder does not exist:\n%s\nPlease specify a new folder.', myFolder);
@@ -31,7 +33,7 @@ if ~isfolder(myFolder)
     end
 end
 % Get a list of all files in the folder with the desired file name pattern.
-PNGFolder = 'C:\Users\User\Documents\UNSW\MTRN4230\Git Repo\4230Project\RGBD_Data\Red Cube PNG';
+PNGFolder = 'C:\Users\User\Documents\UNSW\MTRN4230\Git Repo\4230Project\RGBD_Data\PNG Multiple Objects';
 filePattern = fullfile(myFolder, '*.mat'); % Change to whatever pattern you need.
 theFiles = dir(filePattern);
 for k = 1 : length(theFiles)
@@ -43,7 +45,9 @@ for k = 1 : length(theFiles)
     imageArray = load(fullFileName);
     imshow(imageArray.image);  % Display image.
     drawnow; % Force display to update immediately.
+    % change name so that it is saved as a png
     PNGbaseFileName = strrep(baseFileName, '.mat', '.png');
     PNGfullFileName = fullfile(PNGFolder, PNGbaseFileName);
+    % save to the folder
     imwrite(imageArray.image, PNGfullFileName);
 end
