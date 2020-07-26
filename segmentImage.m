9899c5e09546c39fdb323e5d5b874794717c04c8
Lucas Way <lucasway98@gmail.com>
2020-07-27 01:35:25 +1000

    add comments

--- segmentImage.m
+++ segmentImage.m
@@ -24,43 +24,58 @@ classes = {'PNG Multiple Objects'};
 imdatastore = imageDatastore(fullfile(myFolder,... 
     classes), ...
     'LabelSource', 'foldernames', 'FileExtensions', '.png'); 
+% Take a single image
 data = readimage(imdatastore,5);
 img = data;
 
-se = strel('disk',8);
+% Convert to binary image. Set sensitivity to detect the dark blue objects
+% with the black background
 img_grey = rgb2gray(img);
 img_bw = imbinarize(img_grey,'adaptive','ForegroundPolarity','dark','Sensitivity',0.9);
+% separate images more
+se = strel('disk',8);
 img_bw = imclose(img_bw,se);
+% Separate images and remove some noise
 se = strel('disk',6);
 img_bw = imerode(img_bw,se);
+% Create more solid blobs
 img_bw = imfill(img_bw,'holes');
+
 figure(1);
 imshow(img_bw);
 
+% Find centroids
 stats = regionprops(img_bw,'centroid');
+% Draw centroids on image
 figure(2);
 centroids = cat(1,stats.Centroid);
 imshow(img)
 hold on
 plot(centroids(:,1),centroids(:,2),'b*')
 hold off
+% Create separate image for each object
 numObjects = size(centroids, 1);
 centroids = round(centroids);
 for cnt = 1:numObjects
-    topLeft = centroids(cnt,2) - 80; %round((centroids(2,1)/900) * 640)- 100;
-    if (topLeft <= 0)
-        topLeft = 1; end
-    topRight = centroids(cnt,2) + 80; %round((centroids(2,1)/900) * 640)+ 100;
-    if (topRight >= 480)
-        topRight = 480; end    
-    bottomLeft = centroids(cnt,1) - 80; %round((centroids(2,2)/400) * 480) - 100;
-    if (bottomLeft <= 0) 
-        bottomLeft = 1; end    
-    bottomRight = centroids(cnt,1) + 80; %round((centroids(2,2)/400) * 480)+ 100;
-    if (bottomRight >= 640)
-        bottomRight = 640; end
-
-    section = img(topLeft:topRight,bottomLeft:bottomRight,:);
+    % Set values to obtain pixels surrounding centroid position
+    % Set x values
+    xLeft = centroids(cnt,2) - 80; 
+    % additional condition for the edge of the image
+    if (xLeft <= 0)
+        xLeft = 1; end
+    xRight = centroids(cnt,2) + 80; 
+    if (xRight >= 480)
+        xRight = 480; end
+    % Set y values
+    yLeft = centroids(cnt,1) - 80; 
+    if (yLeft <= 0) 
+        yLeft = 1; end    
+    yRight = centroids(cnt,1) + 80; 
+    if (yRight >= 640)
+        yRight = 640; end
+    % Obtain pixels in defines area
+    section = img(xLeft:xRight,yLeft:yRight,:);
+    % Show image
     figure(cnt + 3);
     imshow(section);
 end
