bf096631af8f61bed583079eca34d26f41432055
Olivia Yem <o.yem@student.unsw.edu.au>
2020-07-28 17:03:00 +1000

    Code cleaned up and checks added, Script for test completed

--- ur5.py
+++ ur5.py
@@ -82,64 +82,100 @@ class ur5Robot:
 	#
 	#kinematics
 	def forwardKin(self): #return the position of the start of the end effector/end of link 6 with the current joint positions
-		calcT();
+		self.calcT();
 		p = np.zeros((6,1))
-		p6 = np.array[[0][0][0][1]]	
-		p0 = np.matmul(T,p6)
+		p6 = np.array([[0],[0],[0],[1]])	
+		p0 = np.matmul(self.T,p6)
 		p[:3] = p0[:3]
-		p[4] = m.atan2(T[3,2],T[3,3])
-		p[5] = m.atan2(T[3,1],sqrt(pow(T[3,2],2)+pow(T[3,3],2)))
-		p[6] = m.atan2(T[2,1],T[1,1])
+		p[3] = m.atan2(self.T[3,2],self.T[3,3])
+		p[4] = m.atan2(self.T[3,1],m.sqrt(m.pow(self.T[3,2],2)+m.pow(self.T[3,3],2)))
+		p[5] = m.atan2(self.T[2,1],self.T[1,1])
 		return p
 	def calcT14(self,T06,theta1,theta5,theta6):
-		T10 = np.linalg.inv([[m.cos(theta1),0,m.sin(theta1),0][m.sin(theta1),m.cos(theta1),-1*m.cos(theta1),0][0,1,0,self.d1][0,0,0,1]])
-		T65 = np.linalg.inv([[m.cos(theta6),-1*m.sin(theta6),0,0][m.sin(theta6),0,0,0][0,0,1,self.d6][0,0,0,1]])
-		T54 = np.linalg.inv([[m.cos(theta5),0,m.sin(theta5),0][m.sin(theta5),m.cos(theta5),-1*m.cos(theta5),0][0,1,0,self.d5][0,0,0,1]])
-		T16 = matmul(T10,T06)
-		T64 = matmul(T65,T54)
-		T14 = matmul(T16,T64)
+		T01 = np.array([[m.cos(theta1),0,m.sin(theta1),0],[m.sin(theta1),m.cos(theta1),-1*m.cos(theta1),0],[0,1,0,self.d1],[0,0,0,1]])
+		T56 = np.array([[m.cos(theta6),-1*m.sin(theta6),0,0],[m.sin(theta6),0,0,0],[0,0,1,self.d6],[0,0,0,1]])
+		T45 = np.array([[m.cos(theta5),0,m.sin(theta5),0],[m.sin(theta5),m.cos(theta5),-1*m.cos(theta5),0],[0,1,0,self.d5],[0,0,0,1]])
+		if np.linalg.det(T01)==0 or np.linalg.det(T45)==0 or np.linalg.det(T56)==0:
+			T14 = np.zeros((4,4))
+			T14[0:4,0:4]=np.nan
+		else:
+			T10 = np.linalg.inv(T01)
+			T65 = np.linalg.inv(T56)
+			T54 = np.linalg.inv(T45)
+			T16 = np.matmul(T10,T06)
+			T64 = np.matmul(T65,T54)
+			T14 = np.matmul(T16,T64)
 		return T14
 	def inverseKin(self,x, y, z,roll,pitch,yaw): #return the joint positions for the end of link 6 with target position x,y,z and orientation roll=c,pitch=b,yaw =a
 		#Code here to turn input arguments into a transformation matrix called target
-		target = np.array([[m.cos(yaw)*m.cos(pitch), m.cos(yaw)*m.sin(pitch)*m.sin(roll)-m.sin(yaw)*m.cos(roll), m.cos(yaw)*m.sin(pitch)*m.cos(roll)+m.sin(yaw)*m.sin(roll), x][m.sin(yaw)*m.cos(pitch), m.sin(yaw)*m.sin(pitch)*m.sin(yaw)+m.cos(yaw)*m.cos(roll), m.sin(yaw)*m.sin(pitch)*m.cos(roll)-m.cos(yaw)*m.sin(roll), y][-1*m.sin(pitch), m.cos(pitch)*m.sin(roll), m.cos(pitch)*m.cos(roll), z][0,0,0,1]]) #also 0T6
+		target = np.array([[m.cos(yaw)*m.cos(pitch), m.cos(yaw)*m.sin(pitch)*m.sin(roll)-m.sin(yaw)*m.cos(roll), m.cos(yaw)*m.sin(pitch)*m.cos(roll)+m.sin(yaw)*m.sin(roll), x],[m.sin(yaw)*m.cos(pitch), m.sin(yaw)*m.sin(pitch)*m.sin(yaw)+m.cos(yaw)*m.cos(roll), m.sin(yaw)*m.sin(pitch)*m.cos(roll)-m.cos(yaw)*m.sin(roll), y],[-1*m.sin(pitch), m.cos(pitch)*m.sin(roll), m.cos(pitch)*m.cos(roll), z],[0,0,0,1]]) #also 0T6
 		thetas = np.zeros((6, 8)) #6x8 matrix, each colum represents one possible solution of joint angles
 	#calculate theta1
-		p05 = target-np.array([0][0][-1*self.d6][1])
-		thetas[1,1:4] = np.real(m.atan2(p05(2),p05(1))+m.acos(d4/sqrt(pow(p05(1),2)+pow(p05(2),2)))+m.pi/2)
-		thetas[1,5:8] = np.real(m.atan2(p05(2),p05(1))-m.acos(d4/sqrt(pow(p05(1),2)+pow(p05(2),2)))+m.pi/2)
+		change = np.array([[0],[0],[-1*self.d6],[1]])
+		p05 = np.matmul(target,change)
+		thetas[0,0] = np.real(m.atan2(p05[2],p05[1])+m.acos(self.d4/m.sqrt(m.pow(p05[1],2)+m.pow(p05[2],2)))+m.pi/2)
+		thetas[0,1] = thetas[0,0]
+		thetas[0,2] = thetas[0,0]
+		thetas[0,3] = thetas[0,0]
+		thetas[0,4] = np.real(m.atan2(p05[2],p05[1])-m.acos(self.d4/m.sqrt(m.pow(p05[1],2)+m.pow(p05[2],2)))+m.pi/2)
+		thetas[0,5] = thetas[0,4]
+		thetas[0,6] = thetas[0,4]
+		thetas[0,7] = thetas[0,4]
 	#Calculate theta5
-		thetas[5,1:2] = np.real(m.acos((x*m.sin(thetas[1,1])-y*m.cos(thetas[1,1])-self.d4)/self.d6))
-		thetas[5,3:4] = np.real(-1*m.acos((x*m.sin(thetas[1,1])-y*m.cos(thetas[1,1])-self.d4)/self.d6))
-		thetas[5,5:6] = np.real(m.acos((x*m.sin(thetas[1,5])-y*m.cos(thetas[1,5])-self.d4)/self.d6))
-		thetas[5,7:8] = np.real(-1*m.acos((x*m.sin(thetas[1,5])-y*m.cos(thetas[1,5])-self.d4)/self.d6))
+		if (x*m.sin(thetas[0,0])-y*m.cos(thetas[0,0])-self.d4)/self.d6 <= 1 and (x*m.sin(thetas[0,0])-y*m.cos(thetas[0,0])-self.d4)/self.d6 >= -1:
+			thetas[4,0] = np.real(m.acos((x*m.sin(thetas[0,0])-y*m.cos(thetas[0,0])-self.d4)/self.d6))
+			thetas[4,1] = thetas[4,0]
+			thetas[4,2] = np.real(-1*m.acos((x*m.sin(thetas[0,0])-y*m.cos(thetas[0,0])-self.d4)/self.d6))
+			thetas[4,3] = thetas[4,2]
+		else:
+			thetas[4,0] = np.nan
+			thetas[4,1] = np.nan
+			thetas[4,2] = np.nan
+			thetas[4,3] = np.nan
+		if (x*m.sin(thetas[0,4])-y*m.cos(thetas[0,4])-self.d4)/self.d6 <= 1 and (x*m.sin(thetas[0,4])-y*m.cos(thetas[0,4])-self.d4)/self.d6 >= -1:
+			thetas[4,4] = np.real(m.acos((x*m.sin(thetas[0,4])-y*m.cos(thetas[0,4])-self.d4)/self.d6))
+			thetas[4,5] = thetas[4,4]
+			thetas[4,6] = np.real(-1*m.acos((x*m.sin(thetas[0,4])-y*m.cos(thetas[0,4])-self.d4)/self.d6))
+			thetas[4,7] = thetas[4,6]
+		else:
+			thetas[4,4] = np.nan
+			thetas[4,5] = np.nan
+			thetas[4,6] = np.nan
+			thetas[4,7] = np.nan
 	#calculate theta6
 		t60 = np.linalg.inv(target)
 		for i in range(8):
-			thetas[6,i] = np.real(m.atan2((-1*t60[2,1]*m.sin(thetas[1,i])+t60[2,2]*m.sin(thetas[1,i]))/m.sin(thetas[5,i]),(t60[1,1]*m.sin(thetas[1,i])-t60[1,2]*m.cos(thetas[1,i]))/m.sin(thetas[5,i])))
+			thetas[5,i] = np.real(m.atan2((-1*t60[0,1]*m.sin(thetas[0,i])+t60[1,1]*m.sin(thetas[0,i]))/m.sin(thetas[4,i]),(t60[0,0]*m.sin(thetas[0,i])-t60[0,1]*m.cos(thetas[0,i]))/m.sin(thetas[4,i])))
 	#calculate theta3
 		#columns 1,2
-		T14 = calcT14(target,thetas[1,1],thetas[5,1],thetas[6,1])
-		thetas[3,1] = np.real(m.acos(m.sqrt(pow(T14[1,4],2)+pow(T14[3,4],2))-pow(self.a2,2)-(pow(self.a3,2))/(2*self.a2*self.a3)))
-		thetas[3,2] = -1*thetas[3,1]
+		T14 = self.calcT14(target,thetas[0,1],thetas[4,1],thetas[5,1])
+		thetas[2,0] = np.real(m.acos(m.sqrt(pow(T14[0,3],2)+pow(T14[2,3],2))-pow(self.a2,2)-(pow(self.a3,2))/(2*self.a2*self.a3)))
+		thetas[2,1] = -1*thetas[2,0]
 		#columns 3,4
-		T14 = calcT14(target,thetas[1,4],thetas[5,4],thetas[6,4])
-		thetas[3,3] = np.real(m.acos(m.sqrt(pow(T14[1,4],2)+pow(T14[3,4],2))-pow(self.a2,2)-(pow(self.a3,2))/(2*self.a2*self.a3)))
-		thetas[3,4] = -1*thetas[3,3]
+		T14 = self.calcT14(target,thetas[0,2],thetas[4,2],thetas[5,2])
+		thetas[2,2] = np.real(m.acos(m.sqrt(pow(T14[0,3],2)+pow(T14[2,3],2))-pow(self.a2,2)-(pow(self.a3,2))/(2*self.a2*self.a3)))
+		thetas[2,3] = -1*thetas[2,2]
 		#columns 5,6
-		T14 = calcT14(target,thetas[1,5],thetas[5,5],thetas[6,5])
-		thetas[3,5] = np.real(m.acos(m.sqrt(pow(T14[1,4],2)+pow(T14[3,4],2))-pow(self.a2,2)-(pow(self.a3,2))/(2*self.a2*self.a3)))
-		thetas[3,6] = -1*thetas[3,5]
+		T14 = self.calcT14(target,thetas[0,4],thetas[4,4],thetas[5,4])
+		thetas[2,4] = np.real(m.acos(m.sqrt(pow(T14[0,3],2)+pow(T14[2,3],2))-pow(self.a2,2)-(pow(self.a3,2))/(2*self.a2*self.a3)))
+		thetas[2,5] = -1*thetas[2,4]
 		#columns 7,8
-		T14 = calcT14(target,thetas[1,7],thetas[5,7],thetas[6,7])
-		thetas[3,7] = np.real(m.acos(m.sqrt(pow(T14[1,4],2)+pow(T14[3,4],2))-pow(self.a2,2)-(pow(self.a3,2))/(2*self.a2*self.a3)))
-		thetas[3,8] = -1*thetas[3,8]
+		T14 = self.calcT14(target,thetas[0,7],thetas[4,7],thetas[5,7])
+		thetas[2,6] = np.real(m.acos(m.sqrt(pow(T14[0,3],2)+pow(T14[2,3],2))-pow(self.a2,2)-(pow(self.a3,2))/(2*self.a2*self.a3)))
+		thetas[2,7] = -1*thetas[2,6]
 	#calculate theta2 and theta4
 		for i in range(8):
-			T14 = calcT14(target,thetas[1,i],thetas[5,i],thetas[6,i])
-			thetas[2,i] = np.real(m.atan2(-1*T14[3,4],-1*T14[1,4])-m.asin(-1*self.a3*m.sin(thetas[3,i])/m.sqrt(pow(T14[1,4],2)+pow(T14[3,4],2))))
-			T21 = np.linalg.inv([[m.cos(thetas[2,i]),-1*m.sin(thetas[2,i]),0,self.a2*m.cos(thetas[2,i])][m.sin(thetas[2,i]),0,0,self.a3*m.sin(thetas[2,i])][0,0,1,0][0,0,0,1]])
-			T32 = np.linalg.inv([[m.cos(thetas[3,i]),-1*m.sin(thetas[3,i]),0,self.a3*m.cos(thetas[3,i])][m.sin(thetas[3,i]),0,0,self.a3*m.sin(thetas[3,i])][0,0,1,0][0,0,0,1]])
-			T31 = matmul(T32,T21)
-			T34 = matmul(T31,T14)
-			thetas[4,i] = m.atan2(T34(2,1),T34(1,1))
+			T14 = self.calcT14(target,thetas[0,i],thetas[4,i],thetas[5,i])
+			thetas[1,i] = np.real(m.atan2(-1*T14[2,3],-1*T14[0,3])-m.asin(-1*self.a3*m.sin(thetas[2,i])/m.sqrt(pow(T14[0,3],2)+pow(T14[2,3],2))))
+			T12 = np.array([[m.cos(thetas[1,i]),-1*m.sin(thetas[1,i]),0,self.a2*m.cos(thetas[1,i])],[m.sin(thetas[1,i]),0,0,self.a3*m.sin(thetas[1,i])],[0,0,1,0],[0,0,0,1]])
+			T23 = np.array([[m.cos(thetas[2,i]),-1*m.sin(thetas[2,i]),0,self.a3*m.cos(thetas[2,i])],[m.sin(thetas[2,i]),0,0,self.a3*m.sin(thetas[2,i])],[0,0,1,0],[0,0,0,1]])
+			if np.linalg.det(T12)==0 or np.linalg.det(T23)==0:
+				T34 = np.zeros((4,4))
+				T34[0:4,0:4]=np.nan
+			else:
+				T21 = np.linalg.inv(T12)
+				T32 = np.linalg.inv(T23)
+				T31 = np.matmul(T32,T21)
+				T34 = np.matmul(T31,T14)
+			thetas[3,i] = m.atan2(T34[1,0],T34[0,0])
 		return thetas
--- ur5Test.py
+++ ur5Test.py
@@ -3,8 +3,10 @@
 
 from ur5 import ur5Robot
 import math as m
+import numpy as np
 
 robot = ur5Robot()
+print("TEST 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 robot.setAllTheta(2.08, 1.78, 0.02, 0.66, 1.83, 2.98)
 print("Theta1 is",robot.getTheta1())
 print("Theta2 is",robot.getTheta2())
@@ -12,5 +14,7 @@ print("Theta3 is",robot.getTheta3())
 print("Theta4 is",robot.getTheta4())
 print("Theta5 is",robot.getTheta5())
 print("Theta6 is",robot.getTheta6())
-robot.calcT()
-print("transformation matrix is",robot.T)
+p = robot.forwardKin()
+print("The end effector is at", p)
+thetas = robot.inverseKin(p[0],p[1],p[2],p[3],p[4],p[5])
+print("The possible values of the joint positions are:\n",thetas)
